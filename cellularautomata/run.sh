757005ec5b473787050b71946aed72c3e434e75c
Ben Kern <bkern1@lion.lmu.edu>
2019-04-08 12:13:19 -0700

    yee

--- cellularautomata/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-clear
-cd $(dirname $0)
-cd ..
-javac -d classfiles cellularautomata/CellularAutomata.java
-java -cp classfiles cellularautomata/CellularAutomata
--- mandelbrot/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-clear
-cd $(dirname $0)
-cd ..
-javac -d classfiles mandelbrot/Mandelbrot.java
-java -cp classfiles mandelbrot/Mandelbrot
