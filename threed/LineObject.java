4dfbe1a4325f29a37862b26a5c5b15c4f60b94b4
benjaminjkern <bkern1@lion.lmu.edu>
2019-09-24 03:30:01 -0700

    Made the 3d renderer do better than just ray marching

--- threed/LineObject.java
+++ threed/LineObject.java
@@ -7,41 +7,49 @@ import kern.Tools;
 
 public class LineObject extends Object3D {
 
-	Color color;
-	PointObject start, end;
+    Color color;
+    PointObject start, end;
 
-	public LineObject(Color color, double[] startPos, double[] endPos) {
-		super(startPos);
-		this.color = color;
-		start = new PointObject(color, startPos);
-		end = new PointObject(color, endPos);
-		add(start); add(end);
-	}
+    public LineObject(Color color, double[] startPos, double[] endPos) {
+        super(startPos);
+        this.color = color;
+        start = new PointObject(color, startPos);
+        end = new PointObject(color, endPos);
+        add(start);
+        add(end);
+    }
 
-	public LineObject(Color color, double[] pos, double[] dir, double length) {
-		super(pos);
-		this.color = color;
-		start = new PointObject(color, pos);
-		end = new PointObject(color, Tools.add(pos, Tools.mult(Tools.unitVector(dir), length)));
-		add(start); add(end);
-	}
+    public LineObject(Color color, double[] pos, double[] dir, double length) {
+        super(pos);
+        this.color = color;
+        start = new PointObject(color, pos);
+        end = new PointObject(color, Tools.add(pos, Tools.mult(Tools.unitVector(dir), length)));
+        add(start);
+        add(end);
+    }
 
-	public double[] dirAndMag() {
-		return Tools.subtract(end.pos, start.pos);
-	}
+    public double[] dirAndMag() {
+        return Tools.subtract(end.pos, start.pos);
+    }
 
-	@Override
-	public void wireFrameDraw(Graphics g, LineObject camera, double fov, double stageWidth, double stageHeight) {
-		g.setColor(color);
-		g.drawLine(start.getScreenPos(camera, fov, stageWidth, stageHeight)[0], start.getScreenPos(camera, fov, stageWidth, stageHeight)[1],
-				end.getScreenPos(camera, fov, stageWidth, stageHeight)[0], end.getScreenPos(camera, fov, stageWidth, stageHeight)[1]);
-	}
+    public double[] unitDir() {
+        return Tools.unitVector(dirAndMag());
+    }
 
-	public void setLength(double length) {
-		end = new PointObject(color, Tools.add(start.pos, Tools.mult(Tools.unitVector(dirAndMag()), length)));
-	}
+    @Override
+    public void wireFrameDraw(Graphics g, LineObject camera, double fov, double stageWidth, double stageHeight) {
+        g.setColor(color);
+        g.drawLine(start.getScreenPos(camera, fov, stageWidth, stageHeight)[0],
+                start.getScreenPos(camera, fov, stageWidth, stageHeight)[1],
+                end.getScreenPos(camera, fov, stageWidth, stageHeight)[0],
+                end.getScreenPos(camera, fov, stageWidth, stageHeight)[1]);
+    }
 
-	public void step() {
-		translate(dirAndMag());
-	}
+    public void setLength(double length) {
+        end = new PointObject(color, Tools.add(start.pos, Tools.mult(Tools.unitVector(dirAndMag()), length)));
+    }
+
+    public void step() {
+        translate(dirAndMag());
+    }
 }
--- threed/PointObject.java
+++ threed/PointObject.java
@@ -6,34 +6,33 @@ import kern.Tools;
 
 public class PointObject extends Object3D {
 
-	Color color;
+    Color color;
 
-	public PointObject(Color color, double[] pos) {
-		super(pos);
-		this.color = color;
-	}
+    public PointObject(Color color, double[] pos) {
+        super(pos);
+        this.color = color;
+    }
 
-	public int[] getScreenPos(LineObject camera, double FOV, double stageWidth, double stageHeight) {
-		double[] x = Tools.unitVector(Tools.crossProduct(camera.dirAndMag(), new double[] {0,0,1}));
-		double[] y = Tools.mult(-1, Tools.unitVector(Tools.crossProduct(x, camera.dirAndMag())));
+    public int[] getScreenPos(LineObject camera, double FOV, double stageWidth, double stageHeight) {
+        double[] x = Tools.unitVector(Tools.crossProduct(camera.dirAndMag(), new double[] { 0, 0, 1 }));
+        double[] y = Tools.mult(-1, Tools.unitVector(Tools.crossProduct(x, camera.dirAndMag())));
 
-		double[][] bases = Tools.concatenate(x,y);
+        double[][] bases = Tools.concatenate(x, y);
 
-		double radius = Math.sqrt(stageWidth*stageWidth + stageHeight*stageHeight)/2.;
+        double radius = Math.sqrt(stageWidth * stageWidth + stageHeight * stageHeight) / 2.;
 
-		double[] diff = Tools.subtract(pos, camera.pos);
+        double[] diff = Tools.subtract(pos, camera.pos);
 
-		double[] proj = Tools.unitVector(Tools.mult(bases, diff));
+        double[] proj = Tools.unitVector(Tools.mult(bases, diff));
 
-		double adjAng = Math.acos(Tools.dotProduct(diff, camera.dirAndMag())/Tools.magnitude(diff))/FOV;
+        double adjAng = Math.acos(Tools.dotProduct(diff, camera.dirAndMag()) / Tools.magnitude(diff)) / FOV;
 
-		return new int[] {getScreenPos(stageWidth, radius, 0, adjAng, proj), getScreenPos(stageHeight, radius, 1, adjAng, proj)};
-	}
+        return new int[] { getScreenPos(stageWidth, radius, 0, adjAng, proj),
+                getScreenPos(stageHeight, radius, 1, adjAng, proj) };
+    }
 
-
-	private static int getScreenPos(double stage, double radius, int xy, double adjAng, double[] vector) {
-		return (int) (stage/2. + radius*vector[xy]*adjAng);
-	}
-
+    private static int getScreenPos(double stage, double radius, int xy, double adjAng, double[] vector) {
+        return (int) (stage / 2. + radius * vector[xy] * adjAng);
+    }
 
 }
