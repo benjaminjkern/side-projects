0c9409c4e02e4be01d5e43b739a039a65f1b8422
benjaminjkern <bkern1@lion.lmu.edu>
2023-01-30 21:40:05 -0800

    Slightly more stuff plus babel

--- OffProjects/DumbAIs/index.js
+++ OffProjects/DumbAIs/index.js
@@ -1,11 +1,29 @@
 import { initGraphs } from "./modules/graph";
-import { draw, init, initCanvas, keyHandler, startLoop } from "./modules/scene";
+import {
+    draw,
+    init,
+    initCanvas,
+    keyHandler,
+    playArea,
+    startLoop,
+} from "./modules/scene";
 
+export let canvas;
+export let ctx;
+export let canvas2holder;
 // unused
 // let GRAPH_SORT_BY_SPECIES = false;
 
 window.onload = () => {
-    initCanvas(document, window);
+    canvas = document.getElementById("canvas");
+    ctx = canvas.getContext("2d");
+
+    canvas.width = playArea.width;
+    canvas.height = playArea.height;
+
+    canvas2holder = document.getElementById("canvas2-holder");
+    canvas2holder.style.width = 200;
+    canvas2holder.style.height = 800;
 
     initGraphs(document, window);
 
@@ -14,7 +32,7 @@ window.onload = () => {
 };
 
 window.onresize = () => {
-    draw();
+    draw(ctx);
 };
 
 window.onclick = () => {};
